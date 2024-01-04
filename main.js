42b8b6ce4a53af060728f0c7c3b1f32462596812
chienpv <phamvanbk11@gmail.com>
2024-01-04 17:54:52 +0700

    demo

---
+++ main.js
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `<h1> User Management </h1>`;
