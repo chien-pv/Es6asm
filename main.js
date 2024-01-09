946d000fcbca59556ee20d2810db501c44b65f6d
chienpv <phamvanbk11@gmail.com>
2024-01-09 18:12:12 +0700

    demo list card photo

--- main.js
+++ main.js
@@ -2,24 +2,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import "./style.css";
 
-import Navigo from "navigo"; // When using ES modules.
-
-const router = new Navigo("/");
-function home() {
-  document.querySelector("#main-app").innerHTML = "Home";
+function buildCard(photo) {
+  return `<div class="card col-3">
+  <img src="${photo.thumbnailUrl}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">${photo.title}</p>
+    <div class="btn-group">
+    <a href="#" class="btn btn-sm btn-outline-secondary">View</a>
+    <a href="#" class="btn btn-sm btn-outline-secondary">Edit</a>
+    </div>
+  </div>
+</div>`;
 }
 
-router
-  .on("/", () => {
-    home();
-  })
-  .on("/home", () => {
-    home();
-  })
-  .on("/about", () => {
-    document.querySelector("#main-app").innerHTML = "about";
-  })
-  .on("/contact", () => {
-    document.querySelector("#main-app").innerHTML = "contact";
-  })
-  .resolve();
+fetch("https://jsonplaceholder.typicode.com/photos")
+  .then((response) => response.json())
+  .then((json) => {
+    let datas = "";
+    json.forEach((photo) => {
+      datas += buildCard(photo);
+    });
+
+    document.getElementById("list-photo").innerHTML = datas;
+  });
